e9a24b3e83c8910835d6a1afaa59a5710be212ce
Jaible <105011906+Jaible@users.noreply.github.com>
2024-05-03 19:33:22 -0600

    Controls and lines check

--- JS/game.js
+++ JS/game.js
@@ -1,7 +1,3 @@
-// For this project, we are using p5.js.
-// A library that will help us to draw
-// the graphics of the game.
-
 // Define the size of each grid space
 const gridSpace = 30;
 
@@ -11,12 +7,10 @@ let gridPieces = [];
 let lineFades = [];
 let gridWorkers = [];
 
-// In-game variables
 let currentScore = 0;
 let currentLevel = 1;
 let linesCleared = 0;
 
-// Game loop variables
 let ticks = 0;
 let updateEvery = 15;
 let updateEveryCurrent = 15;
@@ -24,59 +18,56 @@ let fallSpeed = gridSpace * 0.5;
 let pauseGame = false;
 let gameOver = false;
 
-// Define the edges of the game area
+// Define the edges of game area
 const gameEdgeLeft = 150;
 const gameEdgeRight = 450;
 
-// Define the colors for the blocks
+// Define the colors for the pieces
 const colors = [
-    "#dca3ff",
-    "#ff90a0",
-    "#80ffb4",
-    "#ff7666",
-    "#70b3f5",
-    "#b2e77d",
-    "#ffd700"
-]
+    '#dca3ff',
+    '#ff90a0',
+    '#80ffb4',
+    '#ff7666',
+    '#70b3f5',
+    '#b2e77d',
+    '#ffd700',
+];
 
-// Setup will be the funcion we will call
-// from from the begining of the game
+// Setup function called once at beginning
 function setup() {
-    createCanvas(600, 450);
+    createCanvas(600, 540);
 
     // Create a new falling piece
     fallingPiece = new PlayPiece();
     fallingPiece.resetPiece();
 
-    // We set the font for the text
-    textFont("Ubuntu");
+    // Set the font for the text
+    textFont('Ubuntu');
 }
 
-// With this function we will draw every
-// frame of the game.
+// Draw function called repeatedly
 function draw() {
-    // We define the colors we will going
-    // to use in the game
-    const colorDark = "#0d0d0d";
-    const colorLight = "#304550";
-    const colorBackground = "#e1eeb0";
+    // Define colors used in the game
+    const colorDark = '#0d0d0d';
+    const colorLight = '#304550';
+    const colorBackground = '#e1eeb0';
 
-    // We set the color of the background
+    // Set the background color
     background(colorBackground);
 
-    // We draw the right side of the panel
+    // Draw the right side info panel
     fill(25);
     noStroke();
     rect(gameEdgeRight, 0, 150, height);
 
-    // We draw the left side of the panel
+    // Draw the left side info panel
     rect(0, 0, gameEdgeLeft, height);
 
-    // We draw the score rectangle
+    // Draw the score rectangle
     fill(colorBackground);
     rect(450, 80, 150, 70);
 
-    // We draw the next piece rectangle
+    // Draw the next piece rectangle
     rect(460, 405, 130, 130, 5, 5);
 
     // Draw the level rectangle
@@ -91,7 +82,7 @@ function draw() {
     rect(450, 110, 150, 4);
     rect(450, 140, 150, 4);
 
-    // We draw the score banner
+    // Draw the score banner
     fill(colorBackground);
     rect(460, 60, 130, 35, 5, 5);
 
@@ -101,7 +92,11 @@ function draw() {
     stroke(colorLight);
     rect(465, 65, 120, 25, 5, 5);
 
-    // We draw the next piece inner rectangle
+    // Draw the next piece inner rectangle
+    stroke(colorLight);
+    rect(465, 410, 120, 120, 5, 5);
+
+    // Draw the level inner rectangle
     rect(465, 215, 120, 50, 5, 5);
 
     // Draw the lines inner rectangle
@@ -116,51 +111,48 @@ function draw() {
     text("Level", 525, 238);
     text("Lines", 525, 308);
 
-    // Draw the current info
+    // Draw the actual info
     textSize(24);
-    textAlign(CENTER);
+    textAlign(RIGHT);
     text(currentScore, 560, 135);
     text(currentLevel, 560, 260);
     text(linesCleared, 560, 330);
 
     // Draw the game border
     stroke(colorDark);
-    line(gameEdgeRight, 0, gameEdgeLeft, height);
+    line(gameEdgeRight, 0, gameEdgeRight, height);
 
     // Show the falling piece
     fallingPiece.show();
 
-    // Speed up the piece when the down
-    // arrow is pressed
-    if(keyIsDown(DOWN_ARROW)) {
+    // Speed up the falling piece if the down arrow is pressed
+    if (keyIsDown(DOWN_ARROW)) {
         updateEvery = 2;
     } else {
         updateEvery = updateEveryCurrent;
     }
 
-
     // Update the game state
-    if(!pauseGame) {
+    if (!pauseGame) {
         ticks++;
-
-        if(ticks >= updateEvery) {
+        if (ticks >= updateEvery) {
             ticks = 0;
             fallingPiece.fall(fallSpeed);
         }
     }
 
     // Show the grid pieces
-    for(let i = 0; i < gridPieces.length; i++) {
+    for (let i = 0; i < gridPieces.length; i++) {
         gridPieces[i].show();
     }
 
     // Show the fading lines
-    for(let i = 0; i < lineFades.length; i++) {
+    for (let i = 0; i < lineFades.length; i++) {
         lineFades[i].show();
     }
 
     // Process the grid workers
-    if(gridWorkers.length > 0) {
+    if (gridWorkers.length > 0) {
         gridWorkers[0].work();
     }
 
@@ -192,19 +184,17 @@ function draw() {
 
 // Function called when a key is pressed
 function keyPressed() {
-    // R key
-    if(keyCode === 82) {
+    if (keyCode === 82) {
+        // 'R' key
         resetGame();
     }
-
-    if(!pauseGame) {
-        if(keyCode === LEFT_ARROW) {
+    if (!pauseGame) {
+        if (keyCode === LEFT_ARROW) {
             fallingPiece.input(LEFT_ARROW);
-        } else if(keyCode === RIGHT_ARROW) {
+        } else if (keyCode === RIGHT_ARROW) {
             fallingPiece.input(RIGHT_ARROW);
         }
-
-        if(keyCode === UP_ARROW) {
+        if (keyCode === UP_ARROW) {
             fallingPiece.input(UP_ARROW);
         }
     }
@@ -218,7 +208,7 @@ class PlayPiece {
         this.nextPieceType = Math.floor(Math.random() * 7);
         this.nextPieces = [];
         this.pieceType = 0;
-        this.pieces = 0;
+        this.pieces = [];
         this.orientation = [];
         this.fallen = false;
     }
@@ -226,31 +216,31 @@ class PlayPiece {
     // Generate the next piece
     nextPiece() {
         this.nextPieceType = pseudoRandom(this.pieceType);
-        this.nextPiece = [];
+        this.nextPieces = [];
 
         const points = orientPoints(this.nextPieceType, 0);
         let xx = 525, yy = 490;
 
-        if(this.nextPieceType !== 0 && this.nextPieceType !== 3 && this.nextPieceType !== 5) {
+        if (this.nextPieceType !== 0 && this.nextPieceType !== 3 && this.nextPieceType !== 5) {
             xx += (gridSpace * 0.5);
         }
 
-        if(this.nextPieceType == 5) {
+        if (this.nextPieceType == 5) {
             xx -= (gridSpace * 0.5);
         }
 
-        for(let i = 0; i < 4; i++) {
-            this.nextPieces.push(new Square(xx + points[i][0] * gridSpace, yy + points[i][1] * gridSpace, this.nextPieceType))
+        for (let i = 0; i < 4; i++) {
+            this.nextPieces.push(new Square(xx + points[i][0] * gridSpace, yy + points[i][1] * gridSpace, this.nextPieceType));
         }
     }
 
     // Make the piece fall
     fall(amount) {
-        if(this.futureCollision(0, amount, this.rotation)) {
+        if (!this.futureCollision(0, amount, this.rotation)) {
             this.addPos(0, amount);
             this.fallen = true;
         } else {
-            if(!this.fallen) {
+            if (!this.fallen) {
                 pauseGame = true;
                 gameOver = true;
             } else {
@@ -267,6 +257,7 @@ class PlayPiece {
         this.pos.y = -60;
 
         this.pieceType = this.nextPieceType;
+
         this.nextPiece();
         this.newPoints();
     }
@@ -277,76 +268,238 @@ class PlayPiece {
         this.orientation = points;
         this.pieces = [];
 
-        for(let i = 0; i < points.length; i++) {
-            this.pieces.push(new Square(this.pos.x + points[i][0] * gridSpace,
-                                        this.pos.y + points[i][1] * gridSpace,
-                                        this.pieceType));
+        for (let i = 0; i < points.length; i++) {
+            this.pieces.push(new Square(this.pos.x + points[i][0] * gridSpace, this.pos.y + points[i][1] * gridSpace, this.pieceType));
         }
     }
 
     // Update the position of the current piece
     updatePoints() {
-        if(this.pieces) {
+        if (this.pieces) {
             const points = orientPoints(this.pieceType, this.rotation);
             this.orientation = points;
-
-            for(let i = 0; i < 4; i++) {
+            for (let i = 0; i < 4; i++) {
                 this.pieces[i].pos.x = this.pos.x + points[i][0] * gridSpace;
                 this.pieces[i].pos.y = this.pos.y + points[i][1] * gridSpace;
             }
         }
     }
 
+    // Add an offset to the position of current piece
     addPos(x, y) {
         this.pos.x += x;
         this.pos.y += y;
 
-        if(this.pieces) {
-            for(let i = 0; i < 4; i++) {
+        if (this.pieces) {
+            for (let i = 0; i < 4; i++) {
                 this.pieces[i].pos.x += x;
                 this.pieces[i].pos.y += y;
             }
         }
     }
 
-    // Check if there will be a collision in the
-    // future
+    // Check if there will be a collision in the future
     futureCollision(x, y, rotation) {
         let xx, yy, points = 0;
-
-        if(rotation !== this.rotation) {
+        if (rotation !== this.rotation) {
             points = orientPoints(this.pieceType, rotation);
         }
 
-        for(let i = 0; i < this.pieces.length; i++) {
-            if(points) {
+        for (let i = 0; i < this.pieces.length; i++) {
+            if (points) {
                 xx = this.pos.x + points[i][0] * gridSpace;
-                xx = this.pos.y + points[i][1] * gridSpace;
+                yy = this.pos.y + points[i][1] * gridSpace;
             } else {
-                xx = this.points[i].pos.x + x;
-                xx = this.points[i].pos.y + y;
+                xx = this.pieces[i].pos.x + x;
+                yy = this.pieces[i].pos.y + y;
             }
-
-            if(xx < gameEdgeLeft || xx + gridSpace > gameEdgeRight
-                || yy + gridSpace > height) {
-                    return true;
+            if (xx < gameEdgeLeft || xx + gridSpace > gameEdgeRight || yy + gridSpace > height) {
+                return true;
             }
-
-            for(let j = 0; j < gridPieces.length; j++) {
-                if(xx === gridPieces[j].pos.x) {
-                    if(yy >= gridPieces[j].pos.y &&
-                        yy < gridPieces[j].pos.y + gridSpace) {
-                            return true;
+            for (let j = 0; j < gridPieces.length; j++) {
+                if (xx === gridPieces[j].pos.x) {
+                    if (yy >= gridPieces[j].pos.y && yy < gridPieces[j].pos.y + gridSpace) {
+                        return true;
                     }
-
-                    if(yy + gridSpace > gridPieces[j].pos.y
-                        && yy + gridSpace <= gridPieces[j].pos.y + gridSpace) {
-                            return true;
+                    if (yy + gridSpace > gridPieces[j].pos.y && yy + gridSpace <= gridPieces[j].pos.y + gridSpace) {
+                        return true;
                     }
                 }
             }
         }
     }
 
-    // Handle user inputs
+    // Handle user input
+    input(key) {
+        switch (key) {
+            case LEFT_ARROW:
+                if (!this.futureCollision(-gridSpace, 0, this.rotation)) {
+                    this.addPos(-gridSpace, 0);
+                }
+                break;
+            case RIGHT_ARROW:
+                if (!this.futureCollision(gridSpace, 0, this.rotation)) {
+                    this.addPos(gridSpace, 0);
+                }
+                break;
+            case UP_ARROW:
+                let newRotation = this.rotation + 1;
+                if (newRotation > 3) {
+                    newRotation = 0;
+                }
+                if (!this.futureCollision(0, 0, newRotation)) {
+                    this.rotation = newRotation;
+                    this.updatePoints();
+                }
+                break;
+        }
+    }
+
+    // Rotate the current piece
+    rotate() {
+        this.rotation += 1;
+        if (this.rotation > 3) {
+            this.rotation = 0;
+        }
+        this.updatePoints();
+    }
+
+    // Show the current piece
+    show() {
+        for (let i = 0; i < this.pieces.length; i++) {
+            this.pieces[i].show();
+        }
+        for (let i = 0; i < this.nextPieces.length; i++) {
+            this.nextPieces[i].show();
+        }
+    }
+
+    // Commit the current shape to the grid
+    commitShape() {
+        for (let i = 0; i < this.pieces.length; i++) {
+            gridPieces.push(this.pieces[i]);
+        }
+        this.resetPiece();
+        analyzeGrid();
+    }
+}
+
+// Class for each square in a piece
+class Square {
+    constructor(x, y, type) {
+        this.pos = createVector(x, y);
+        this.type = type;
+    }
+
+    // Show the square
+    show() {
+        strokeWeight(2);
+        const colorDark = '#092e1d';
+        const colorMid = colors[this.type];
+
+        fill(colorMid);
+        stroke(25);
+        rect(this.pos.x, this.pos.y, gridSpace - 1, gridSpace - 1);
+
+        noStroke();
+        fill(255);
+        rect(this.pos.x + 6, this.pos.y + 6, 18, 2);
+        rect(this.pos.x + 6, this.pos.y + 6, 2, 16);
+        fill(25);
+        rect(this.pos.x + 6, this.pos.y + 20, 18, 2);
+        rect(this.pos.x + 22, this.pos.y + 6, 2, 16);
+    }
+}
+
+// Generate a pseudo-random number for the next piece
+function pseudoRandom(previous) {
+    let roll = Math.floor(Math.random() * 8);
+    if (roll === previous || roll === 7) {
+        roll = Math.floor(Math.random() * 7);
+    }
+    return roll;
+}
+
+// Analyze the grid and clear lines if necessary
+function analyzeGrid() {
+    let score = 0;
+    while (checkLines()) {
+        score += 100;
+        linesCleared += 1;
+        if (linesCleared % 10 === 0) {
+            currentLevel += 1;
+            if (updateEveryCurrent > 2) {
+                updateEveryCurrent -= 10;
+            }
+        }
+    }
+    if (score > 100) {
+        score *= 2;
+    }
+    currentScore += score;
+}
+
+// Check if there are any complete lines in the grid
+function checkLines() {
+    for (let y = 0; y < height; y += gridSpace) {
+        let count = 0;
+        for (let i = 0; i < gridPieces.length; i++) {
+            if (gridPieces[i].pos.y === y) {
+                count++;
+            }
+        }
+        if (count === 10) {
+            // Remove the pieces at this y-coordinate
+            gridPieces = gridPieces.filter(piece => piece.pos.y !== y);
+            // Move down the pieces above this y-coordinate
+            for (let i = 0; i < gridPieces.length; i++) {
+                if (gridPieces[i].pos.y < y) {
+                    gridPieces[i].pos.y += gridSpace;
+                }
+            }
+            return true;
+        }
+    }
+    return false;
+}
+
+// Class for the grid worker
+class Worker {
+    constructor(y, amount) {
+        this.amountActual = 0;
+        this.amountTotal = amount;
+        this.yVal = y;
+    }
+
+    // Perform work on the grid
+    work() {
+        if (this.amountActual < this.amountTotal) {
+            for (let j = 0; j < gridPieces.length; j++) {
+                if (gridPieces[j].pos.y < y) {
+                    gridPieces[j].pos.y += 5;
+                }
+            }
+            this.amountActual += 5;
+        } else {
+            gridWorkers.shift();
+        }
+    }
+}
+
+// Reset the game state
+function resetGame() {
+    fallingPiece = new PlayPiece();
+    fallingPiece.resetPiece();
+    gridPieces = [];
+    lineFades = [];
+    gridWorkers = [];
+    currentScore = 0;
+    currentLevel = 1;
+    linesCleared = 0;
+    ticks = 0;
+    updateEvery = 15;
+    updateEveryCurrent = 15;
+    fallSpeed = gridSpace * 0.5;
+    pauseGame = false;
+    gameOver = false;
 }
